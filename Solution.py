edd9a3a726c6de3f2f35ba2dac88e08428cef5d8
Maksim Akimov <a.t.n20034@gmail.com>
2024-06-05 23:36:01 +0700

    init

--- Solution.py
+++ Solution.py
@@ -5,7 +5,6 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid or not grid[0]:
             return 0
-
         islands = 0
         visit = set()
         rows, cols = len(grid), len(grid[0])
